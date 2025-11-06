e11227cdde4cee44c38b875e2b91e2c74ebf2818
Raghib Najmi <84832906+ragOP@users.noreply.github.com>
2025-11-06 18:17:43 +0530

    taj

--- src/App.jsx
+++ src/App.jsx
@@ -13,6 +13,7 @@ import Raghib2 from './Raghib2';
 import Raghib3 from "./Raghib3";
 import Ok from './Ok';
 import Tester from './Tester'
+import Taj from './taj'
 
 const App = () => {
   // sds
@@ -27,6 +28,7 @@ const App = () => {
         {/* <Route path="/engsf2200dup" element={<ChatbotFour />} />
         <Route path="/engsafe1" element={<Chatbot2 />} /> */}
         <Route path="/" element={<Raghib />} />
+         <Route path="taj" element={<Taj />} />
         {/* <Route path="/raghib" element={<Raghib3 />} /> */}
          <Route path="/raghib" element={<Raghib2 />} />
           <Route path="/newdes1" element={<Ok />} />
